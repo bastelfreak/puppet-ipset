99b4f26e24b6ee961a5cbe6ac5af11d9dccee5a3
Tim Meusel <tim@bastelfreak.de>
2019-08-15 14:40:43 +0200

    rewrite init.pp

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,3 +1,15 @@
-class ipset {
-
+class ipset (
+  String[1] $package,
+  String[1] $service,
+  Stdlib::Ensure::Service $service_ensure,
+  Boolean $enable,
+  Enum['present', 'absent', 'latest'] $package_ensure,
+){
+  package{$ipset::package:
+    ensure => $package_ensure,
+  }
+  service{$service:
+    ensure => $service_ensure,
+    enable => $enable,
+  }
 }
